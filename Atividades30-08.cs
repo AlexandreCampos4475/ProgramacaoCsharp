d512e5b1693b07feab3e05df86ea21d8c98248e4
alexandre4475 <128434953+AlexandreCampos4475@users.noreply.github.com>
2023-08-30 21:35:26 -0300

    Add files via upload

---
+++ Atividades30-08.cs
@@ -0,0 +1,45 @@
+
+/* Crie um programa em C# que realize o seguinte:
+
+Solicite ao usuário que insira seu nome completo.
+Solicite ao usuário que insira sua idade.
+Utilize métodos de string para realizar as seguintes operações:
+Exiba o nome completo em maiúsculas.
+Exiba o nome completo em minúsculas.
+Conte o número de caracteres no nome completo (ignorando espaços em branco).
+Converta a idade fornecida pelo usuário em um número inteiro.
+Calcule o ano de nascimento do usuário com base na idade fornecida (supondo que o ano atual seja 2023).
+Exiba o ano de nascimento calculado e a idade em que o usuário fará 100 anos.
+Dicas:
+
+Use os métodos ToUpper(), ToLower(), Length, Trim() e conversões para manipular as strings.
+Use a classe Convert para converter a idade fornecida em um número inteiro.
+Lembre-se de que o ano de nascimento pode ser calculado subtraindo a idade do ano atual (2023). */
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Insira seu nome completo: ");
+        string nomeCompleto = Console.ReadLine();
+
+        Console.Write("Insira sua idade: ");
+        string idadeInput = Console.ReadLine();
+        int idade = Convert.ToInt32(idadeInput);
+
+        string nomeMaiusculo = nomeCompleto.ToUpper();
+        string nomeMinusculo = nomeCompleto.ToLower();
+        int numeroCaracteres = nomeCompleto.Replace(" ", "").Length;
+
+        int anoNascimento = 2023 - idade;
+        int idadeCemAnos = 100 - idade;
+
+        Console.WriteLine("Nome completo em maiúsculas: " + nomeMaiusculo);
+        Console.WriteLine("Nome completo em minúsculas: " + nomeMinusculo);
+        Console.WriteLine("Número de caracteres no nome completo: " + numeroCaracteres);
+        Console.WriteLine("Ano de nascimento calculado: " + anoNascimento);
+        Console.WriteLine("Você fará 100 anos em: " + (2023 + idadeCemAnos));
+    }
+}
