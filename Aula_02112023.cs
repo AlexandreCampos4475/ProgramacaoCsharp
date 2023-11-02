3d4ba36710bdd57a3a4deeda60d84749826f72f5
alexandre4475 <128434953+AlexandreCampos4475@users.noreply.github.com>
2023-11-01 21:02:47 -0300

    Add files via upload

---
+++ Aula_02112023.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileStream_CriarArquivo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream ("C:\\Users\\Sala\\Documents\\C#", FileMode.Create , FileMode.Append);
+            byte[] bdata = Encoding.Default.GetBytes("Ola, eu sei escrever um arquivo..");
+fs.Write(bdata, 0, bdata.Length);
+            fs.Close();
+            Console.Write("O arquivo foi criado com sucesso:");
+            Console.ReadKey();
+        }
+    }
+}
