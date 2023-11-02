448a5c22332a9e1cc9b768a2e17a53c92c779aee
alexandre4475 <128434953+AlexandreCampos4475@users.noreply.github.com>
2023-11-01 21:05:59 -0300

    Add files via upload

---
+++ Exercicio4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace atividades
+{
+    class exercicio04
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite sua nacionalidade: ");
+            string nacionalidade = Console.ReadLine();
+
+            if (idade >= 16 && nacionalidade == "brasileiro")
+            {
+                Console.WriteLine("Você é elegível para votar!");
+            }
+            else
+            {
+                Console.WriteLine("Você não é elegível para votar!");
+            }
+        }
+    }
+}
---
+++ exercicio5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome completo:");
+        string nomeCompleto = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade:");
+        string idade = Console.ReadLine();
+
+        Console.WriteLine($"Nome completo em maiúsculas: {nomeCompleto.ToUpper()}");
+        Console.WriteLine($"Nome completo em minúsculas: {nomeCompleto.ToLower()}");
+        Console.WriteLine($"Número de caracteres no nome completo: {nomeCompleto.Trim().Length}");
+
+        int idadeInteiro = Convert.ToInt32(idade);
+        int anoNascimento = 2023 - idadeInteiro;
+
+        Console.WriteLine($"Ano de nascimento: {anoNascimento}");
+        Console.WriteLine($"Idade em que fará 100 anos: {100 - idadeInteiro}");
+    }
+}
